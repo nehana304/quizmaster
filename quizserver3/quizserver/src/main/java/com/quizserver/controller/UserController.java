c0944934909c323e57f1eb70dad06428b501737c
nehana304 <nehana304@gmail.com>
2025-10-30 17:36:06 +0530

    edit cors

--- quizserver3/quizserver/src/main/java/com/quizserver/controller/UserController.java
+++ quizserver3/quizserver/src/main/java/com/quizserver/controller/UserController.java
@@ -29,7 +29,6 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4201", "http://localhost:3000", "http://127.0.0.1:4200", "http://127.0.0.1:4201"}, allowCredentials = "true")
 public class UserController {
 
     @Autowired
